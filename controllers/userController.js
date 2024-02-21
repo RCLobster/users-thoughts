6854050458b6468b83c123bdc31e3381853fa72d
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-02-20 16:01:02 -0800

    able to GET single user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
 const { Users, Thought } = require('../models');
 
 module.exports = {
+    // GET all users
     async getUsers(req, res) {
         try {
             const users = await Users.find();
+            res.json(users);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    // GET single user by id
+    async getSingleUser(req, res) {
+        try {
+            const users = await Users.findById({ _id: req.params.userId });
             res.json(users);
         } catch(err) {
             res.status(500).json(err);
